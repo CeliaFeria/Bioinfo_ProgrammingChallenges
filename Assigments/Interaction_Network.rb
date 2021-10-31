74cd9c75b06b8da9f83e93042730077045225a1b
CeliaFeria <celiaferiadelgado99@gmail.com>
2021-10-31 15:17:12 -0400

    almost

---
+++ Assigments/Interaction_Network.rb
@@ -0,0 +1,39 @@
+
+require "./Web_information.rb"
+class Network
+  
+:Interactions
+:GO_info
+:KEGG_info 
+
+#@@Interaction_Network = []
+@interacctions = Hash.new
+  
+  
+
+  
+def initialize (params={})
+    @Interaction = params.fetch(:Interactions, "ABC")
+    @GO_info = params.fetch(:GO_info, "ABC")
+    @KEGG_info = params.fetch(:KEGG_info, "ABC")
+  
+end
+  
+  
+def Network.information(info)
+  info.each do |key, value|
+  all = Network.new({:Interactions => info[key], 
+    :GO_info => get_GO(key), 
+    :KEGG_info =>  get_KEGG_info(key)})
+      
+    @interacctions[key] = all
+  end
+end 
+  
+  def Network.all_info
+    return @interacctions
+  end
+  
+end 
+
+
---
+++ Assigments/main.rb
@@ -0,0 +1,5 @@
+
+require "./Interaction_Network.rb"
+Network.information(get_Network(get_interacction(get_AGI_Locus('./ArabidopsisSubNetwork_GeneList.txt'))))
+
+Network.all_info
