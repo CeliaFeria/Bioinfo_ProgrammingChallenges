30d099f0424163528c8a6bdb2428e250e6ee16e6
CeliaFeria <90108736+CeliaFeria@users.noreply.github.com>
2021-11-09 11:05:02 -0500

    Delete main.rb

--- Assigments/main.rb
+++
@@ -1,9 +0,0 @@
-
-require "./Network.rb"
-require "./Web_info.rb"
-
-Network.information(get_Network(get_complex_interactions(get_AGI_Locus('./ArabidopsisSubNetwork_GeneList.txt'))))
-Network.all_info
-Network.report
-
-
